ac33a9721a677517b6f2b8aea20bdb0a77da861b
amani-alnobani <amanialnobani@gmail.com>
2021-07-05 16:26:04 +0300

    add Aboutme html project

---
+++ js/app.js
@@ -0,0 +1,58 @@
+'use strict';
+let user = prompt('what is your name?')
+alert('HELLO '+ user)
+alert('we going to play gussing game, Answer with yes or no. let us start.')
+let v1= prompt('do you think I am an Engineer?')
+v1=v1.toLowerCase();
+if (v1=='yes'){
+alert('that\'s correct')
+    
+}
+
+else {
+    alert('that\'s wrong')
+}
+
+let v2= prompt('do you think I am married?')
+v1=v2.toLowerCase();
+if (v2=='yes'){
+alert('that\'s correct')
+    
+}
+
+else {
+    alert('that\'s wrong')
+}
+
+let v3 = prompt('do you think I have an baby?')
+v3 =v3.toLowerCase();
+if (v3=='yes'){
+alert('that\'s correct')
+    
+}
+
+else {
+    alert('that\'s wrong')
+}
+
+let v4= prompt('do you think that i like reading?')
+v4=v4.toLowerCase();
+if (v4=='yes'){
+alert('that\'s correct')
+    
+}
+
+else {
+    alert('that\'s wrong')
+}
+
+let v5= prompt('would you like to know more about me?')
+v5=v5.toLowerCase();
+if (v5=='yes'){
+alert('ok!, see you soon '+user)
+    
+}
+
+else {
+    alert('nice to meet you '+user)
+}
