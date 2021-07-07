55b76d4d6e84d6a409ecb732be8f1f43551ee9aa
amani-alnobani <amanialnobani@gmail.com>
2021-07-07 15:01:32 +0300

    edit

--- js/app.js
+++ js/app.js
@@ -1,13 +1,16 @@
 'use strict';
 
-let score = 7
+let score = 0;  
 let user = prompt('what is your name?')
 alert('HELLO '+ user)
 alert('we going to play gussing game, Answer with yes or no. let us start.')
+
+
 let v1= prompt('do you think I am an Engineer?')
 v1=v1.toLowerCase();
 if (v1==='yes'||v1==='y'){
 alert('that\'s correct')
+score++;
     
 }else if(v1==='no'||v1==='n'){
     
@@ -23,7 +26,7 @@ let v2= prompt('do you think I am married?')
 v1=v2.toLowerCase();
 if (v2==='yes'||v2==='y'){
 alert('that\'s correct')
-    
+score++;
 }
 else if(v2==='no'||v2==='n'){
     
@@ -36,7 +39,7 @@ let v3 = prompt('do you think I have an baby?')
 v3 =v3.toLowerCase();
 if (v3==='yes'||v3==='y'){
 alert('that\'s correct')
-    
+score++;
 }
 
 else if(v3==='no'||v3==='n'){
@@ -52,7 +55,7 @@ let v4= prompt('do you think that i like reading?')
 v4=v4.toLowerCase();
 if (v4=='yes'||v4==='y'){
 alert('that\'s correct')
-    
+score++;  
 }
 else if(v4==='no'||v4==='n'){
     
@@ -69,6 +72,7 @@ v5=v5.toLowerCase();
 if (v5==='yes'||v5==='y'){
 
     alert('ok!, see you soon '+user)
+    score++;
     
 }
 else if(v5==='no'||v5==='n'){
@@ -76,63 +80,158 @@ else if(v5==='no'||v5==='n'){
     alert('nice to meet you '+user);
 }
 else {
-    alert('please aswer with yes/y or no/n')
-}
-let index = prompt('try to enter the right number, you have only 4 attempt.') ;
+    alert('please aswer with yes/y or no/n')}
+
+
+
+let index = prompt('try to enter the right number between 0 to 10, you have only 4 attempt.');
+// let c = 0;
+let rn = 5
+rn=parseInt(rn)
 for (let i = 0; i < 4; i++) {
-    if (index === '5') {
+    
+    // c++;
+    if (index == rn) {
         alert('correct!')
-        break
+        break;
+        score++;
     }
-    else{
+    else if (index > rn ){
 
-        index=prompt('try again!')
+        index =prompt('too high try again!');
+
+    }else if(index < rn){
+
+        index=prompt('too low try again')
+
+    }else {
+
+        alert('pleas write a number')
+    }
+    if (i == 3){
+        alert('you run out of trial the correct number is 5')
+    
+
     }
 }
-    alert('the right answer was number 5')
+// if (c>4){
+
+// console.log(c);
+
+// }
+// if (i == 3){
+//     alert('you run out of trial the correct number is 5')
 
 
 
-    let hobbies=['reading','skiing','swimming','fix things'];
+    
 
-    console.log(hobbies.length);
-let t1 = prompt('whats my favourit hobbies?,write down one of them. you have 6 attempt!.');
-for (let i = 0; i < 5; i++) {
-    let score = 0
-    for (let j = 0; j < hobbies.length; j++) {
-         
-        switch (t1) {
-            case 'reading':
-                alert('right')
-                break;
-            case 'skiing':
-                alert('right')
-                break;
 
-            case 'swimming':
-                alert('right')
-                break;
+// let t1 = '';
+//     let hobbies=['reading','skiing','swimming','fix things'];
 
-            case 'fix things':
-                alert('right')
-                break;
+// //     console.log(hobbies.length);
+//  t1 = prompt('whats my favourit hobbies?,write down one of them. you have 6 attempt!.');
+// for (let i = 0; i < 5; i++) {
+//     for(let j = 0; j<hobbies.length; j++){
+//         if(hobbies[i] === t1)
+//         alert('correct');
+//         j=10;
 
-            default:
-               t1=prompt('try again')
-                break;
-            
-        }
+//     }
+//     if(i===5){
+//     alert('out of trials the answer is' + hobbies)
+// }
+// else if(i<6){
+
+//     alert ('wrong answer')
+// }
+
+
+
+
+
+
+let trial='';
+let hobbies =['reading','skiing','swimming','fix things'];
+
+
+// let flag=false;
+for (let j = 0; j <6; j++) {
+    trial=prompt('guess one of my hobbies');
+
+    for (let i = 0; i < hobbies.length; i++) {
         
-        break;
-    }
-    
-    if (t1 === 'reading' || t1 === 'skiing' || t1 === 'swimming' || t1 === 'fix things' ) {
-        break;
-    }
-    
-}
-alert('my hobbies are [ reading , skiing , swimming , fix things]')
+        console.log(hobbies[i]);
+        if (hobbies[i]===trial) {
+            alert('correct, and here are all the answers '+hobbies);
+            
+            score++;
+            j=10;
+        }
 
- alert('you have a score of '+ score+'/7')
+    }
+    if (j===5) {
+        alert('sorry no more tries '+hobbies);
+    }else if(j<6){
+        alert('wrong answer');
+    }
+
+
+}
+
+
+alert(`thank you ${user}, your score is ${score}/7`);
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+         
+//         switch (t1) {
+//             case 'reading':
+//                 alert('right')
+//                 break;
+//             case 'skiing':
+//                 alert('right')
+//                 break;
+
+//             case 'swimming':
+//                 alert('right')
+//                 break;
+
+//             case 'fix things':
+//                 alert('right')
+//                 break;
+
+//             default:
+//                t1=prompt('try again')
+//                 break;
+            
+//         }
+        
+//         break;
+//     }
+    
+//     if (t1 === 'reading' || t1 === 'skiing' || t1 === 'swimming' || t1 === 'fix things' ) {
+//         break;
+//     }
+    
+// }
+// alert('my hobbies are [ reading , skiing , swimming , fix things]')
+
+//  alert('you have a score of '+ score+'/7')
 
 
